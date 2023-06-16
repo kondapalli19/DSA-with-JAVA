418fa48558eb2be96170d9d9e526c2a91f736a4b
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-06-16 13:24:34 +0530

    Update Main.java

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -1,20 +1,63 @@
 public class Main {
     public static void main(String[] args) {
-        SLL list=new SLL();
-        list.insertFirst(23);
-        list.insertFirst(43);
-        list.insertFirst(90);
-        list.insertFirst(100);
-        list.insertLast(78);
-        list.insertLast(12);
-        list.insert(56, 2);
+
+        // SLL list=new SLL();
+
+        // list.insertFirst(23);
+        // list.insertFirst(43);
+        // list.insertFirst(90);
+        // list.insertFirst(100);
+
+        // list.insertLast(78);
+        // list.insertLast(12);
+
+        // list.insert(56, 2);
+
+        // list.display();
+
+        // System.out.println(list.deleteFirst());
+
+        // list.display();
+
+        // System.out.println(list.deleteLast());
+
+        // list.display();
+
+        // System.out.println(list.delete(2));
+
+        // list.display();
+        
+        // System.out.println(list.find(23));
+
+        // DLL list=new DLL();
+
+        // list.insertFirst(23);
+        // list.insertFirst(43);
+        // list.insertFirst(90);
+        // list.insertFirst(100);
+        // list.insertLast(94);
+        // list.insert(2, 54);
+
+        // list.display();
+
+        // System.out.println(list.deleteFirst());
+        // System.out.println(list.deleteLast());
+        // System.out.println(list.delete(2));
+
+        // list.display();
+
+        CLL list=new CLL();
+
+        list.insert(89);
+        list.insert(9);
+        list.insert(78);
+        list.insert(45);
+        list.insert(50);
+        
         list.display();
-        System.out.println(list.deleteFirst());
+
+        list.delete(9);
+
         list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(2));
-        list.display();
-        System.out.println(list.find(23));
     }
 }
