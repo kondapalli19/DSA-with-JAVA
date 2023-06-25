38d785325701674f45c306fffefad0d209b36e41
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-06-25 09:30:22 +0530

    Update TreeMain.java

--- Binary Tree/TreeMain.java
+++ Binary Tree/TreeMain.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class TreeMain {
     public static void main(String[] args) {
 
-        Scanner scanner=new Scanner(System.in);
-        BinaryTree tree=new BinaryTree();
-        tree.populate(scanner);
+        // Scanner scanner=new Scanner(System.in);
+        // BinaryTree tree=new BinaryTree();
+        // tree.populate(scanner);
+        // tree.display();
+        // tree.prettyDisplay();
+
+        BST tree=new BST();
+        int[] nums={1,9,0,6,7,4,5,3};
+        tree.populate(nums);
         tree.display();
-        tree.prettyDisplay();
     }
 }
