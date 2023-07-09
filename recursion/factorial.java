ec30572c29c5875f0afbd565a4db98806cf09c77
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-07-09 20:43:07 +0530

    Create factorial.java

---
+++ recursion/factorial.java
@@ -0,0 +1,17 @@
+public class Factorial {
+    public static int factorial(int n) {
+        // Base case: factorial of 0 or 1 is 1
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        // Recursive case: factorial of n is n multiplied by factorial of (n-1)
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
