95b45a3a3684fc20a4eafb09c633b0aab6da4491
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-09-25 00:03:56 +0530

    Create MaximumNumberOfKSumPairs.java

---
+++ LeetCode75/TwoPointers/MaximumNumberOfKSumPairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+        int count=0;
+        while(left<right){
+            if(nums[left]+nums[right]==k){
+                count++;
+                left++;
+                right--;            }
+        
+        else if(nums[left]+nums[right]<k){
+            left++;
+        }
+        else right--;
+}
+return count;
+    }
+}
