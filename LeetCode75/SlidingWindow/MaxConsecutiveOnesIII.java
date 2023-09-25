b077827a396725cf3e410ab2f0d257d193bd4952
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-09-25 19:56:13 +0530

    Create MaxConsecutiveOnesIII.java

---
+++ LeetCode75/SlidingWindow/MaxConsecutiveOnesIII.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i=0, j=0;
+        while(i<nums.length){
+            if(nums[i]==0) k--;
+            if(k<0){
+                if(nums[j]==0){
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+        return i-j;
+    }
+}
