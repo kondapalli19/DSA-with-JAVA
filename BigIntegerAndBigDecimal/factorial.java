9f9113b9cc88605af43423d5e24a1c17de64dd58
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-10-01 00:06:10 +0530

    Create factorial.java

---
+++ BigIntegerAndBigDecimal/factorial.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class factorial {
+    static BigInteger fact(int num){
+        BigInteger ans=new BigInteger("1");
+
+        for (int i = 2; i <= num; i++) {
+            ans=ans.multiply(BigInteger.valueOf(i));
+        }
+        return ans;
+    }
+}
