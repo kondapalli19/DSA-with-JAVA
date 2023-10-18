7ea3dfd51e16958289a055abace8d9c2ad2a4171
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-10-18 22:38:54 +0530

    Create KthLargeElementInArray.java

---
+++ LeetCode75/HeapOrPriorityQueue/KthLargeElementInArray.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
+        
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+        
+        return minHeap.peek();
+    }
+}
