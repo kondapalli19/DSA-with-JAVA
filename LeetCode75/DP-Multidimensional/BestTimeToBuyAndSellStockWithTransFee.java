bbf8f42237c859f5a9a385967690f763bafb0d8b
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-10-24 20:58:26 +0530

    Create BestTimeToBuyAndSellStockWithTransFee.java

---
+++ LeetCode75/DP-Multidimensional/BestTimeToBuyAndSellStockWithTransFee.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
+
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
+        }
+
+        return sell;
+    }
+}
