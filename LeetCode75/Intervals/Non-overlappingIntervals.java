1c273f247f13a48fc01cb6a738891b8040984eba
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-10-24 22:48:22 +0530

    Create Non-overlappingIntervals.java

---
+++ LeetCode75/Intervals/Non-overlappingIntervals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int prev = 0;
+        int count = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] >= intervals[prev][1]) {
+                prev = i;
+                count++;
+            }
+        }
+        return n - count;
+    }
+}
