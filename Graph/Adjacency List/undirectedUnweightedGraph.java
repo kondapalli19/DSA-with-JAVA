3c7bae79c0e81872bffc987340e901fa6ef6d2a5
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2023-11-30 18:53:12 +0530

    Create undirectedUnweightedGraph.java

---
+++ Graph/Adjacency List/undirectedUnweightedGraph.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class undirectedUnweightedGraph{
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[], int v){
+        for(int i=0; i<graph.length; i++){
+            graph[i]=new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,2));
+    }
+}
